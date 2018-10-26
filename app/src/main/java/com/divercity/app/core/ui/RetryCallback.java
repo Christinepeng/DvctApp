14a8162016d16dafd4dafe993bddf91a69a17b91
Lucas Griotto <lucasgriotto@hotmail.com>
2018-10-26 18:04:24 -0300

    App deleted

--- app/src/main/java/com/divercity/app/core/ui/RetryCallback.java
+++
@@ -1,5 +0,0 @@
-package com.divercity.app.core.ui;
-
-public interface RetryCallback {
-    void retry();
-}
--- app/src/main/java/com/divercity/app/data/networking/config/ErrorUtils.java
+++
@@ -1,8 +0,0 @@
-package com.divercity.app.data.networking.config;
-
-/**
- * Created by lucas on 18/10/2018.
- */
-
-public class ErrorUtils {
-}
